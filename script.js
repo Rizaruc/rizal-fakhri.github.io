09f50e17cc1025e146df4debfaf9763ba4b81c7f
rizalfakhri <rizalfakrhi018@gmail.com>
2025-03-16 16:00:13 +0700

    perbarui

--- script.js
+++ script.js
@@ -39,6 +39,22 @@ window.onscroll = () => {
 
 
 /*==================== scroll reveal ====================*/
-
-
+ScrollReveal({
+    reset: true,
+    distance: '80px',
+    duration: 2000,
+    delay: 200
+});
+ 
+ScrollReveal().reveal('.home-content, .heading', { origin: 'top' });
+ScrollReveal().reveal('.home-img, .services-container, .certificate-box, .contact form', { origin: 'bottom' });
+ScrollReveal().reveal('.home-content h1, .about-image', { origin: 'left' });
+ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right' });
 /*==================== typed js ====================*/
+const typed = new Typed('.multiple-text', {
+    strings: ['Game Developer', 'Game Developer', 'Game Developer'],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
